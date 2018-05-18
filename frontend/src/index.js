621b53a44c50f5141c953f6846b23ac39b838ec3
Thomas Collins <tcollins@hubspot.com>
2018-05-18 10:20:52 +0100

    content api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
 
 import Main from './components/Main'
+import LocalizationContext from './utils/context';
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
@@ -13,10 +14,15 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 })
 
+
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Main />
-  </ApolloProvider>, 
+  
+    <ApolloProvider client={client}>
+      <LocalizationContext.Provider>
+        <Main />
+      </LocalizationContext.Provider>
+    </ApolloProvider>
+  , 
   document.getElementById('root')
 )
 registerServiceWorker()
